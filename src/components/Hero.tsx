d2d8b1f7156358e33e24f25a4893ecfc7fd9945a
Roberth Strand <me@robstr.dev>
2024-11-05 23:27:31 +0100

    fix: small changes to flavor-text
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,8 +25,8 @@ export function Hero() {
           <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
             {[
               ['Speakers', '30'],
-              ['People Attending', '300'],
-              ['Venue', 'Rebell'],
+              ['People Attending', '350'],
+              ['Venue', 'Rebel'],
               ['Location', 'Oslo, Norway'],
             ].map(([name, value]) => (
               <div key={name}>
